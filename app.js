748a33ca36e819ce1e3e479300e36c22206e8873
Jadepypy <questiongirl829@gmail.com>
2021-10-24 16:14:26 +0800

    temp

--- app.js
+++ app.js
@@ -4,32 +4,55 @@ const { Server } = require("socket.io");
 const app = express()
 const httpServer = createServer(app);
 const io = new Server(httpServer, {cors: ['localhost:3000']});
-
+let revisionID = 0
 app.use(express.static('public'))
 
 let doc = ''
 
 io.on("connection", (socket) => {
+  socket.emit('init', {id: revisionID, doc: doc})
   socket.on('operation', (op) => {
-    console.log(op)
-    doc = applyOperation(doc, op)
-    socket.broadcast.emit("operation", op);
+    revisionID++
+    doc = applyOperation(doc, op[0])
+    socket.emit('ack', revisionID)
+    socket.broadcast.emit('syncOperation', {id: revisionID, op: op});
   })
 });
 
 function applyOperation(doc, operation) {
-  switch (operation[0]) {
+  switch (operation.opType) {
     case 'insert' :
-      doc = doc.substring(0, operation[1]) + operation[2] + doc.substring(operation[1], doc.length)
+      doc = doc.substring(0, operation.pos) + operation.key + doc.substring(operation.pos, doc.length)
       break;
     case 'delete' :
-      doc = doc.substring(0, operation[1] + operation[2]) + doc.substring(operation[1], doc.length)
+      doc = doc.substring(0, operation.pos + operation.count) + doc.substring(operation.pos, doc.length)
       break;
   }
   return doc
 }
 
+function transformation(op1, op2){
+  if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.INSERT){
+    if (op1.pos >= op2.pos) {
+      op1.pos += 1
+    } else {
+      op2.pos += 1
+    }
+    return [op1, op2]
+  } else if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.DELETE){
+    if (op1.pos > op2.pos){
+      op1.pos = op2.pos
+    } else {
 
+    }
+  } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.INSERT){
+
+  } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.DELETE){
+
+  } else {
+
+  }
+}
 httpServer.listen(8080)
 app.listen(3000, () => {
   console.log('Server listening on 3000.')
--- public/js/event.js
+++ public/js/event.js
@@ -1,33 +1,100 @@
 const textarea = document.querySelector('textarea')
 const specialKeys = ['Alt', 'Shift', 'Meta', 'Control', 'CapsLock', 'Tab', 'ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowRight']
-const socket = io('http://localhost:8080');
+const socket = io('http://localhost:8080')
+let outstandingOp = []
+let bufferOp = []
+let revisionID
+const STATE = {
+  CLEAR: 0,
+  WAITING: -1  
+}
+const OP_TYPE = {
+  INSERT: 0,
+  DELETE: 1,
+  NOOP: -1
+}
+let state = STATE.CLEAR
 
 textarea.addEventListener('keydown', function (event) {
   let key = event.key == 'Enter'? '\n' : event.key
   const indexStart = textarea.selectionStart;
   const indexEnd = textarea.selectionEnd;
   if (specialKeys.includes(key)) return
-  if (key == "Backspace"){
-    const deleteInfo = ['delete', indexEnd, Math.min(indexStart - indexEnd, -1)]
-    socket.emit('operation', deleteInfo)
+  let opInfo
+  if (key == 'Backspace'){
+    opInfo = {opType: OP_TYPE.DELETE, pos: indexEnd, count: Math.min(indexStart - indexEnd, -1)}
   } else {
-    const insertInfo = ['insert', indexStart, key]
-    socket.emit('operation', insertInfo)
+    opInfo = {opType: OP_TYPE.INSERT, pos: indexStart, key: key}
+  }
+  if (state == STATE.CLEAR){
+    outstandingOp.push(opInfo)
+    socket.emit('operation', outstandingOp) 
+    state = STATE.WAITING
+  } else {
+    bufferOp.push(opInfo)
   }
 });
 
-socket.on('operation', (op) => {
-  textarea.value = applyOperation(textarea.value, op)
+socket.on('init', (init) => {
+  console.log(init)
+  revisionID = init.id
+  textarea.value = init.doc
+})
+
+socket.on('ack', (id) => {
+  console.log('acknowledment')
+  console.log(id)
+  revisionID = id
+  state = STATE.CLEAR
+  if (bufferOp.length > 0){
+    socket.emit('operation', bufferOp) 
+    state = STATE.WAITING
+    pendingOp = []
+  }
+})
+
+socket.on('syncOperation', (syncOp) => {
+  revisionID = syncOp.id
+
+  textarea.value = applyOperation(textarea.value, syncOp.op[0])
 })
 
 function applyOperation(doc, operation) {
-  switch (operation[0]) {
-    case 'insert' :
-      doc = doc.substring(0, operation[1]) + operation[2] + doc.substring(operation[1], doc.length)
+  switch (operation.opType) {
+    case OP_TYPE.INSERT :
+      doc = doc.substring(0, operation.pos) + operation.key + doc.substring(operation.pos, doc.length)
       break;
-    case 'delete' :
-      doc = doc.substring(0, operation[1] + operation[2]) + doc.substring(operation[1], doc.length)
+    case OP_TYPE.DELETE :
+      doc = doc.substring(0, operation.pos + operation.count) + doc.substring(operation.pos, doc.length)
       break;
   }
   return doc
 }
+
+function iterateOperation () {
+
+}
+
+function transformation(op1, op2){
+  if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.INSERT){
+    if (op1.pos > op2.pos) {
+      op1.pos += 1
+    } else {
+      op2.pos += 1
+    }
+    return [op1, op2]
+  } else if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.DELETE){
+    if (op1.pos > op2.pos){
+      op1.pos = op2.pos
+    } else {
+
+    }
+  } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.INSERT){
+
+  } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.DELETE){
+
+  } else {
+
+  }
+}
+
