2f051baaf0d1affcd435d2446c28ce8bb6713731
Jadepypy <questiongirl829@gmail.com>
2021-10-25 20:53:32 +0800

    complete opeational transformation implementation with unknown minor bugs to fix

--- app.js
+++ app.js
@@ -5,54 +5,172 @@ const app = express()
 const httpServer = createServer(app);
 const io = new Server(httpServer, {cors: ['localhost:3000']});
 let revisionID = 0
+const LogOp = []
 app.use(express.static('public'))
+const OP_TYPE = {
+  INSERT: 0,
+  DELETE: 1,
+  NOOP: -1
+}
 
 let doc = ''
 
 io.on("connection", (socket) => {
   socket.emit('init', {id: revisionID, doc: doc})
-  socket.on('operation', (op) => {
-    revisionID++
-    doc = applyOperation(doc, op[0])
-    socket.emit('ack', revisionID)
-    socket.broadcast.emit('syncOperation', {id: revisionID, op: op});
+  socket.on('operation', (info) => {
+    setTimeout(() => {
+      console.log("SERVER INFO:", info)
+      if (revisionID > info.revisionID) {
+        for (let i = info.revisionID + 1; i < LogOp.length; i++){
+          if (LogOp[i]){
+            //change info.operation inplace
+            iterateOT(LogOp[i], info.operation)
+          } 
+        }
+      }
+      revisionID++
+      doc = applyOperation(doc, info.operation)
+      console.log('pending...')
+      socket.emit('ack', revisionID)
+      socket.broadcast.emit('syncOperation', {id: revisionID, syncOp: info.operation});
+      console.log('SEND SYNC:', info.operation)
+      LogOp[revisionID] = info.operation
+    }, 6000)
   })
 });
 
+//Time Complexity: O(N*M), seems inevitable
+function iterateOT (opArr1, opArr2) {
+  let opArr1Prime = []
+  //let opArr2Prime = [...opArr2]
+  for (let op1 of opArr1){
+    for (let i =0; i < opArr2.length; i++){
+      let op2
+      [op1, op2] = transformation(op1, opArr2[i])
+      console.log(op1)
+      if (Array.isArray(op2)){
+        opArr2[i++] = op2[0]
+        opArr2[i] = op2[1] 
+      } else{
+        opArr2[i] = op2    
+      }
+    }
+    if (Array.isArray(op1)){
+      opArr1Prime.push(op1[0])
+      opArr1Prime.push(op1[1]) 
+    } else{
+      opArr1Prime.push(op1)
+    }
+  }
+  return opArr1Prime
+}
+
 function applyOperation(doc, operation) {
-  switch (operation.opType) {
-    case 'insert' :
-      doc = doc.substring(0, operation.pos) + operation.key + doc.substring(operation.pos, doc.length)
-      break;
-    case 'delete' :
-      doc = doc.substring(0, operation.pos + operation.count) + doc.substring(operation.pos, doc.length)
-      break;
+  for (const op of operation){
+    switch (op.opType) {
+      case OP_TYPE.INSERT :
+        doc = doc.substring(0, op.pos) + op.key + doc.substring(op.pos, doc.length)
+        break;
+      case OP_TYPE.DELETE :
+        doc = doc.substring(0, op.pos + op.count) + doc.substring(op.pos, doc.length)
+        break;
+    }
   }
   return doc
 }
 
+
+
+//heart of OT
 function transformation(op1, op2){
   if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.INSERT){
-    if (op1.pos >= op2.pos) {
-      op1.pos += 1
-    } else {
-      op2.pos += 1
-    }
-    return [op1, op2]
+    return Tii(op1, op2)
   } else if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.DELETE){
-    if (op1.pos > op2.pos){
-      op1.pos = op2.pos
-    } else {
-
-    }
+    return Tid(op1, op2)
   } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.INSERT){
-
+    const result = Tid(op2, op1)
+    return [result[1], result[0]]
   } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.DELETE){
-
-  } else {
-
+    return Tdd(op1, op2)
   }
+  return [op1, op2]
 }
+
+//insert  insert transformation
+function Tii(op1, op2){
+  if (op1.pos >= op2.pos) {
+    op1.pos += 1
+  } else {
+    op2.pos += 1
+  }
+  return [op1, op2]
+}
+//insert delete transformation
+function Tid(op1, op2){
+  const op1Temp = {...op1}
+  if (op1.pos > op2.pos + op2.count){
+    op1.pos = Math.max(op2.pos + op2.count, op1Temp.pos + op2.count)
+    if (op1Temp.pos < op2.pos) {
+      const op2First = {...op2}
+      op2First.pos += 1
+      op2First.count = (op1Temp.pos + 1) - op2First.pos
+      const op2Second = {opType: OP_TYPE.DELETE, pos: op1Temp.pos, count: op2.count - op2First.count} 
+      op2 = [op2First, op2Second]
+    }
+  } else {
+    op2.pos += 1
+  }
+  return [op1, op2]
+}
+//delete delete transformation
+function Tdd(op1, op2){
+  if (op1.pos == op2.pos){
+    if (op1.count == op2.count){
+      op1.opType = OP_TYPE.NOOP
+      op2.opType = OP_TYPE.NOOP
+    } else if (Math.abs(op1.count) > Math.abs(op2.count)){
+      op1.pos = op2.pos + op2.count
+      op1.count = op1.count - op2.count
+      op2.opType = OP_TYPE.NOOP     
+    } else {
+      op2.pos = op1.pos + op1.count
+      op2.count = op2.count - op1.count
+      op1.opType = OP_TYPE.NOOP
+    }
+  } else if (op1.pos + op1.count < op2.pos && op1.pos > op2.pos) {
+    const op2Temp = {...op2}
+    if (op2.pos + op2.count >= op1.pos + op1.count){
+      op2.opType = OP_TYPE.NOOP
+      op1.pos = op1.pos + op2Temp.count
+      op1.count = op1.count - op2Temp.count 
+    } else {
+      op2.pos = op1.pos + op1.count
+      op2.count = (op2Temp.pos + op2Temp.count) - (op1.pos + op1.count)
+      op1.count = op2Temp.pos - op1.pos
+      op1.pos = op1.pos + op2Temp.count
+    }
+  } else if (op2.pos + op2.count < op1.pos && op2.pos > op1.pos) {
+    const op1Temp = {...op1}
+    if (op1.pos + op1.count >= op2.pos + op2.count){
+      op1.opType = OP_TYPE.NOOP
+      op2.pos = op2.pos + op1Temp.count
+      op2.count = op2.count - op1Temp.count 
+    } else {
+      op1.pos = op2.pos + op2.count
+      op1.count = (op1Temp.pos + op1Temp.count) - (op2.pos + op2.count)
+      op2.count = op1Temp.pos - op2.pos
+      op2.pos = op2.pos + op1Temp.count
+    }
+  } else if (op1.pos > op2.pos) {
+    op1.pos = op1.pos + op2.count
+  } else {
+    op2.pos = op2.pos + op1.count
+  }
+  console.log('delete delete----')
+  console.log(op1, op2)
+  return [op1, op2]
+}
+
 httpServer.listen(8080)
 app.listen(3000, () => {
   console.log('Server listening on 3000.')
--- public/js/event.js
+++ public/js/event.js
@@ -1,6 +1,7 @@
 const textarea = document.querySelector('textarea')
 const specialKeys = ['Alt', 'Shift', 'Meta', 'Control', 'CapsLock', 'Tab', 'ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowRight']
 const socket = io('http://localhost:8080')
+
 let outstandingOp = []
 let bufferOp = []
 let revisionID
@@ -17,84 +18,193 @@ let state = STATE.CLEAR
 
 textarea.addEventListener('keydown', function (event) {
   let key = event.key == 'Enter'? '\n' : event.key
-  const indexStart = textarea.selectionStart;
-  const indexEnd = textarea.selectionEnd;
+  const indexStart = textarea.selectionStart
+  const indexEnd = textarea.selectionEnd
   if (specialKeys.includes(key)) return
-  let opInfo
+  let opInfo = []
   if (key == 'Backspace'){
-    opInfo = {opType: OP_TYPE.DELETE, pos: indexEnd, count: Math.min(indexStart - indexEnd, -1)}
+    opInfo.push({opType: OP_TYPE.DELETE, pos: indexEnd, count: Math.min(indexStart - indexEnd, -1)})
+    console.log('opinfo:', opInfo)
   } else {
-    opInfo = {opType: OP_TYPE.INSERT, pos: indexStart, key: key}
+    //console.log("INSERT:", OP_TYPE.INSERT, indexStart, key)
+    if (indexEnd - indexStart > 0){
+      opInfo.push({opType: OP_TYPE.DELETE, pos: indexEnd, count: indexStart - indexEnd})
+    }
+    opInfo.push({opType: OP_TYPE.INSERT, pos: indexStart, key: key})
   }
-  if (state == STATE.CLEAR){
-    outstandingOp.push(opInfo)
-    socket.emit('operation', outstandingOp) 
+  if (state === STATE.CLEAR){
+    outstandingOp.push(...opInfo)
+    console.log('OUTSTANDING:', outstandingOp)
+    socket.emit('operation', {revisionID, operation: outstandingOp}) 
     state = STATE.WAITING
   } else {
-    bufferOp.push(opInfo)
+    bufferOp.push(...opInfo)
   }
 });
 
 socket.on('init', (init) => {
-  console.log(init)
+  //console.log(init)
   revisionID = init.id
   textarea.value = init.doc
 })
 
 socket.on('ack', (id) => {
-  console.log('acknowledment')
-  console.log(id)
+  //console.log('acknowledment')
   revisionID = id
   state = STATE.CLEAR
+  outstandingOp = []
   if (bufferOp.length > 0){
-    socket.emit('operation', bufferOp) 
+    //console.log('send operatoin')
+    //console.log(bufferOp)
+    socket.emit('operation', {revisionID, operation: bufferOp})
+    outstandingOp = [...bufferOp]
     state = STATE.WAITING
-    pendingOp = []
+    bufferOp = []
   }
 })
 
-socket.on('syncOperation', (syncOp) => {
-  revisionID = syncOp.id
-
-  textarea.value = applyOperation(textarea.value, syncOp.op[0])
+socket.on('syncOperation', (syncInfo) => {
+  revisionID = syncInfo.id
+  console.log('sync:', syncInfo)
+  if (outstandingOp.length > 0){
+    //change syncOp inplace
+    //console.log(outstandingOp)
+    iterateOT([...outstandingOp], syncInfo.syncOp)
+  }
+  if (bufferOp.length > 0){
+    //change syncOp inplace
+    console.log('buffer not empty')
+    bufferOp = iterateOT(bufferOp, syncInfo.syncOp)
+  }
+  textarea.value = applyOperation(textarea.value, syncInfo.syncOp)
 })
 
 function applyOperation(doc, operation) {
-  switch (operation.opType) {
+  for (const op of operation){
+    switch (op.opType) {
     case OP_TYPE.INSERT :
-      doc = doc.substring(0, operation.pos) + operation.key + doc.substring(operation.pos, doc.length)
+      doc = doc.substring(0, op.pos) + op.key + doc.substring(op.pos, doc.length)
       break;
     case OP_TYPE.DELETE :
-      doc = doc.substring(0, operation.pos + operation.count) + doc.substring(operation.pos, doc.length)
+      doc = doc.substring(0, op.pos + op.count) + doc.substring(op.pos, doc.length)
       break;
+    }
   }
   return doc
 }
-
-function iterateOperation () {
-
+//Time Complexity: O(N*M), seems inevitable
+function iterateOT (opArr1, opArr2) {
+  let opArr1Prime = []
+  //let opArr2Prime = [...opArr2]
+  for (let op1 of opArr1){
+    for (let i =0; i < opArr2.length; i++){
+      let op2
+      [op1, op2] = transformation(op1, opArr2[i])
+      if (Array.isArray(op2)){
+        opArr2[i++] = op2[0]
+        opArr2[i] = op2[1] 
+      } else{
+        opArr2[i] = op2    
+      }
+    }
+    if (Array.isArray(op1)){
+      opArr1Prime.push(op1[0])
+      opArr1Prime.push(op1[1]) 
+    } else{
+      opArr1Prime.push(op1)
+    }
+  }
+  return opArr1Prime
 }
 
+
+
+//heart of OT
 function transformation(op1, op2){
   if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.INSERT){
-    if (op1.pos > op2.pos) {
-      op1.pos += 1
-    } else {
-      op2.pos += 1
-    }
-    return [op1, op2]
+    return Tii(op1, op2)
   } else if (op1.opType == OP_TYPE.INSERT && op2.opType == OP_TYPE.DELETE){
-    if (op1.pos > op2.pos){
-      op1.pos = op2.pos
-    } else {
-
-    }
+    return Tid(op1, op2)
   } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.INSERT){
-
+    const result = Tid(op2, op1)
+    return [result[1], result[0]]
   } else if (op1.opType == OP_TYPE.DELETE && op2.opType == OP_TYPE.DELETE){
-
-  } else {
-
+    return Tdd(op1, op2)
   }
+  return [op1, op2]
 }
 
+//insert  insert transformation
+function Tii(op1, op2){
+  if (op1.pos > op2.pos) {
+    op1.pos += 1
+  } else {
+    op2.pos += 1
+  }
+  return [op1, op2]
+}
+//insert delete transformation
+function Tid(op1, op2){
+  const op1Temp = {...op1}
+  if (op1.pos > op2.pos + op2.count){
+    op1.pos = Math.max(op2.pos + op2.count, op1Temp.pos + op2.count)
+    if (op1Temp.pos < op2.pos) {
+      const op2First = {...op2}
+      op2First.pos += 1
+      op2First.count = (op1Temp.pos + 1) - op2First.pos
+      const op2Second = {opType: OP_TYPE.DELETE, pos: op1Temp.pos, count: op2.count - op2First.count} 
+      op2 = [op2First, op2Second]
+    }
+  } else {
+    op2.pos += 1
+  }
+  return [op1, op2]
+}
+//delete delete transformation
+function Tdd(op1, op2){
+  if (op1.pos == op2.pos){
+    if (op1.count == op2.count){
+      op1.opType = OP_TYPE.NOOP
+      op2.opType = OP_TYPE.NOOP
+    } else if (Math.abs(op1.count) > Math.abs(op2.count)){
+      op1.pos = op2.pos + op2.count
+      op1.count = op1.count - op2.count
+      op2.opType = OP_TYPE.NOOP     
+    } else {
+      op2.pos = op1.pos + op1.count
+      op2.count = op2.count - op1.count
+      op1.opType = OP_TYPE.NOOP
+    }
+  } else if (op1.pos + op1.count < op2.pos && op1.pos > op2.pos) {
+    const op2Temp = {...op2}
+    if (op2.pos + op2.count >= op1.pos + op1.count){
+      op2.opType = OP_TYPE.NOOP
+      op1.pos = op1.pos + op2Temp.count
+      op1.count = op1.count - op2Temp.count 
+    } else {
+      op2.pos = op1.pos + op1.count
+      op2.count = (op2Temp.pos + op2Temp.count) - (op1.pos + op1.count)
+      op1.count = op2Temp.pos - op1.pos
+      op1.pos = op1.pos + op2Temp.count
+    }
+  } else if (op2.pos + op2.count < op1.pos && op2.pos > op1.pos) {
+    const op1Temp = {...op1}
+    if (op1.pos + op1.count >= op2.pos + op2.count){
+      op1.opType = OP_TYPE.NOOP
+      op2.pos = op2.pos + op1Temp.count
+      op2.count = op2.count - op1Temp.count 
+    } else {
+      op1.pos = op2.pos + op2.count
+      op1.count = (op1Temp.pos + op1Temp.count) - (op2.pos + op2.count)
+      op2.count = op1Temp.pos - op2.pos
+      op2.pos = op2.pos + op1Temp.count
+    }
+  } else if (op1.pos > op2.pos) {
+    op1.pos = op1.pos + op2.count
+  } else {
+    op2.pos = op2.pos + op1.count
+  }
+  //console.log('delete delete----')
+  //console.log(op1, op2)
+  return [op1, op2]
+}
